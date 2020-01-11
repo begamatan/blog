b5a1f5a5a179dc9c2d472061447d24b5e2e51658
Agung <agungptr97@gmail.com>
2020-01-12 00:58:59 +0700

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect('blog');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('blog')->group(function () {
+    Route::get('/', 'BlogController@getPosts')->name('blog.index');
+    Route::middleware('Canvas\Http\Middleware\ViewThrottle')->get('{slug}', 'BlogController@findPostBySlug')->name('blog.post');
+    Route::get('tag/{slug}', 'BlogController@getPostsByTag')->name('blog.tag');
+    Route::get('topic/{slug}', 'BlogController@getPostsByTopic')->name('blog.topic');
+});
